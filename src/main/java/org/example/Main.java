0ba848de90eba79b85e112e07ebabdf593111a41
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-03-12 12:19:03 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.Text;
+
+public class Main {
+    public static void main(String[] args) {
+        Text text = new Text("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/Text.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Text {
+    String text;
+
+    Text(String t) {
+        this.text = t;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void cleanText() {
+        removeComments();
+    }
+
+    private void removeComments() {
+        this.text = this.text.lines().filter(line -> !line.startsWith("//")).reduce("", String::concat);
+    }
+
+    private void normalizeWhiteSpaces() {
+        this.text = this.text.replaceAll(" +", " ");
+    }
+
+    private void removeDuplicateWords() {
+        String regex = "\\b(\\w+)((?:\\W+\\1\\b)+)";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(this.text);
+
+        while (m.find()) {
+            this.text = this.text.replaceAll(m.group(), m.group(2));
+        }
+    }
+}
