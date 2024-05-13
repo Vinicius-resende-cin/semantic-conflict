9e51805d1caa57c361aca1007aa104e0e94893a3
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-05-13 18:19:56 -0300

    Add oa conflict base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import samples.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        OverridingAssignmentLocalVariablesSample.conflict();
     }
 }
