2f2b792930ad61bcef374ce71e3b6081a525c933
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-05-13 18:37:38 -0300

    Add df conflict base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import samples.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        DFPBaseSample dfpBaseSample = new DFPBaseSample();
+        dfpBaseSample.setText("Hello  World");
+        dfpBaseSample.cleanText();
+        System.out.println(dfpBaseSample.getText());
     }
 }
