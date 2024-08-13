f5d21e28ad11467c45d06b79a682581a568b992d
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-08-13 16:27:17 -0300

    Add jimple optimization base

---
+++ src/main/java/samples/JimpleOptimization.java
@@ -0,0 +1,10 @@
+package samples;
+
+public class JimpleOptimization {
+    public static void conflict() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
