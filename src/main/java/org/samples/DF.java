60712d93f83663c97bc28a6be03ce0e350459c5f
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-12-16 18:37:16 -0300

    Add simple df left

--- src/main/java/org/samples/DF.java
+++ src/main/java/org/samples/DF.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class DF {
     public static void conflict() {
         HashMap map = new HashMap();
-        map.put("map1", "map1");
+        map.put("map1", "map1-left");
         map.put("map2", "map2");
         map.put("map3", "map3");
         map.put("map4", "map4");
