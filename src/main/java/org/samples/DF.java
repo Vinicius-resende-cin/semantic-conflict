86eb6fc5c7f24400e37a89981f922c5baf1747df
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-12-16 18:00:46 -0300

    Add simple df base

---
+++ src/main/java/org/samples/DF.java
@@ -0,0 +1,14 @@
+package org.samples;
+
+import java.util.HashMap;
+
+public class DF {
+    public static void conflict() {
+        HashMap map = new HashMap();
+        map.put("map1", "map1");
+        map.put("map2", "map2");
+        map.put("map3", "map3");
+        map.put("map4", "map4");
+        map.put("map5", "map5");
+    }
+}
