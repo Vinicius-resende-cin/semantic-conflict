ff660d3750c194a1635ab436b4b3eb3e507e6f53
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-12-26 20:56:51 -0300

    Add left oa files alt

--- src/main/java/org/samples/LeftAssignment.java
+++ src/main/java/org/samples/LeftAssignment.java
@@ -2,6 +2,6 @@ package org.samples;
 
 public class LeftAssignment {
     public static void sum2(Foo bar) {
-        bar.x += 2;
+        bar.x += 3;
     }
 }
--- src/main/java/org/samples/OAInterSample.java
+++ src/main/java/org/samples/OAInterSample.java
@@ -3,6 +3,7 @@ package org.samples;
 public class OAInterSample {
     static Foo bar = new Foo();
     public static void conflict() {
+        LeftAssignment.sum2(bar);
         System.out.println(bar.x);
     }
 }
