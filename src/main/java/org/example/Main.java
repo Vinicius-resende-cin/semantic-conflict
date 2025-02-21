b53b394fd6e0a40ce2d043274ce8065a0cb35375
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-02-21 13:07:15 -0300

    Add cf base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class Main {
+    static int x = 0;
+    static int y = 0;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(x);
+        System.out.println(y);
+        int z = x + y;
+        System.out.println(z);
+    }
+
+    public static void add1toX() {
+        x++;
+    }
+
+    public static void add1toY() {
+        y++;
     }
 }
