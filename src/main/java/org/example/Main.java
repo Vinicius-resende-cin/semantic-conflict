8b925eeb70e4b337ed9c592046c2888a927db0ff
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-02-25 15:28:03 -0300

    Add cf local base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,10 @@
 package org.example;
 
 public class Main {
-    static int x = 0;
-    static int y = 0;
     public static void main(String[] args) {
-        System.out.println(x);
-        System.out.println(y);
+        int x = 0;
+        int y = 0;
         int z = x + y;
         System.out.println(z);
-    }
-
-    public static void add1toX() {
-        x++;
-    }
-
-    public static void add1toY() {
-        y++;
     }
 }
