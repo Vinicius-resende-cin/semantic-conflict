563dc6bc189996d13da8743bcb38e1b6fde19aee
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-02-25 00:06:14 -0300

    Add cf right

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(x);
         System.out.println(y);
+        add1toY();
         int z = x + y;
         System.out.println(z);
     }
