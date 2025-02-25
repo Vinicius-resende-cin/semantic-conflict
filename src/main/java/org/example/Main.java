5f910a08e4364e59bfc2f094161baac9e0d5bb8c
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-02-25 15:28:39 -0300

    Add cf local left

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int x = 0;
+        x++;
         int y = 0;
         int z = x + y;
         System.out.println(z);
