ba749786f8ad4a849cf5fdf22c06bf7260754969
ykarocin <ysa@cin.ufpe.br>
2025-03-20 16:21:04 -0300

    Adding left conflict

---
+++ src/main/java/org/example/AddOne.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class AddOne {
+    public static int PlusOne(Example var){
+        return var.x++;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     static Example a = new Example();
     static Example b = new Example();
     public static void main(String[] args) {
-
+        AddOne.PlusOne(a); //left
         System.out.println("divider");
         
         int z = a.x + b.x;
