c6a867f70532ba1f02641468ae1d69d5ff867755
ykarocin <ysa@cin.ufpe.br>
2025-03-20 16:20:18 -0300

    Changing cf base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     static Example b = new Example();
     public static void main(String[] args) {
 
+        System.out.println("divider");
         
         int z = a.x + b.x;
         System.out.println(z);
