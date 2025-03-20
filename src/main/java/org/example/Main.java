6f04b14be670c8dff026370bde96add4ca192ccb
ykarocin <ysa@cin.ufpe.br>
2025-03-20 16:22:44 -0300

    Adding right conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("divider");
-        
+        PlusOne.AddOne(b); //right
         int z = a.x + b.x;
         System.out.println(z);
     }
