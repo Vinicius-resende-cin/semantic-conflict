2805e0120ce1c1c43884b1aec8f8462f2e2845b8
ykarocin <ysa@cin.ufpe.br>
2025-03-20 15:17:22 -0300

    Base to a new conflict of cf using calls

--- src/main/java/org/example/AddOne.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class AddOne {
-    public static int plusOne(Example var){
-        return var.x++;
-    }
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ public class Main {
     static Example a = new Example();
     static Example b = new Example();
     public static void main(String[] args) {
-        AddOne.plusOne(a); //left
+
+        
         int z = a.x + b.x;
         System.out.println(z);
     }
