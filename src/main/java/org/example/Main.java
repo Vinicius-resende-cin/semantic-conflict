8bd51d6758b77c850b657c6bb08815dd7122b28e
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-04-03 15:31:20 -0300

    right cf alt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("divider");
-        
+        addY();
         int z = x + y;
         System.out.println(z);
     }
