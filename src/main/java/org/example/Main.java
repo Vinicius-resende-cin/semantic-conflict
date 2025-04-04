5aa05f86a5f0791366222a78f1c3694eb70a75c1
vinicius <vrb@cin.ufpe.br>
2025-04-04 17:55:14 -0300

    add cf simple right

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("divider");
-        
+        b++;
         int z = a + b;
         System.out.println(z);
     }
