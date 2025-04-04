9d1b7a494ae4b476f34bef949fbcbfcb13a8fa16
vinicius <vrb@cin.ufpe.br>
2025-04-04 17:54:33 -0300

    add cf simple left

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     static int a = 1;
     static int b = 1;
     public static void main(String[] args) {
-
+        a++;
         System.out.println("divider");
         
         int z = a + b;
