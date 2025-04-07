a9997d312988e131db18f41b39a3e7e52fc8dd3f
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-04-07 19:55:05 -0300

    add oa alt inter base

--- src/main/java/org/samples/OAInterSample.java
+++ src/main/java/org/samples/OAInterSample.java
@@ -3,8 +3,6 @@ package org.samples;
 public class OAInterSample {
     static Foo bar = new Foo();
     public static void conflict() {
-        LeftAssignment.sum2(bar);
         System.out.println(bar.x);
-        RightAssignment.sub1(bar);
     }
 }
