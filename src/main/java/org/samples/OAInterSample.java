5c4a7eec3e8ae2e11bed6876d162df6bdb96755b
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-04-09 11:25:53 -0300

    add right

--- src/main/java/org/samples/OAInterSample.java
+++ src/main/java/org/samples/OAInterSample.java
@@ -6,6 +6,7 @@ public class OAInterSample {
     public static void conflict() {
         OAInterSample.subConflict(bar);
         System.out.println(bar.x);
+        RightAssignment.sub1(bar);
     }
 
     public static void subConflict(Foo bar) {
