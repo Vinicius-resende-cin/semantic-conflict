9820bdbde1789c618f151081631b7d8e51c6fe66
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-04-09 11:20:16 -0300

    add base

--- src/main/java/org/samples/OAInterSample.java
+++ src/main/java/org/samples/OAInterSample.java
@@ -2,7 +2,13 @@ package org.samples;
 
 public class OAInterSample {
     static Foo bar = new Foo();
+
     public static void conflict() {
+        OAInterSample.subConflict(bar);
+        System.out.println(bar.x);
+    }
+
+    public static void subConflict(Foo bar) {
         System.out.println(bar.x);
     }
 }
