4de37f728c13503431714759874b6c320bd3843b
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-05-20 22:03:43 -0300

    prev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String input = "the the dog";
         Text t = new Text(input);
+        t.normalizeWhiteSpace();
         t.removeComments();
         System.out.println(t.getText());
     }
