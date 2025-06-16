339595d47f67fe057653eda6c2ad5ae241efeb93
ykarocin <ysa@cin.ufpe.br>
2025-06-16 14:37:47 -0300

    updating file base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Clue clue = new Clue("Digital printing on the display window", 8);
+        Clue clue = new Clue("Digital printing on the display window", -1);
     }
 }
