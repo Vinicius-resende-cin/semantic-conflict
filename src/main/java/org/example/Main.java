fdd56d2e330592b18bfcdb232ba4321c0f1d8bcf
ykarocin <ysa@cin.ufpe.br>
2025-07-08 01:18:24 -0300

    Setuping base for example 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        
+        Detective sherlock = new Detective("Sherlock Holmes");
+
         CaseFile case1 = new CaseFile("Museum Robbery");
         case1.addClue(new Clue("footprints found on back window", 5));
+        sherlock.addCaseFile(case1);
+        sherlock.solveCase(case1);
         System.out.println("hello world");
     }
 }
