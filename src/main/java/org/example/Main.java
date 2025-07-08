a96eabb26c37e9ebaccbc5ce878a23d042896dd1
ykarocin <ysa@cin.ufpe.br>
2025-07-08 01:02:23 -0300

    updating current changes for example 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         CaseFile case1 = new CaseFile("Museum Robbery");
         case1.addClue(new Clue("footprints found on back window", 5));
         System.out.println("hello world");
+        Clue clueFromCase1 = case1.getNthClue(0); // current change
+
     }
 }
