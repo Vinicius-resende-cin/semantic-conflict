85829812df9d48d1482482dba170ee1953dbee35
ykarocin <ysa@cin.ufpe.br>
2025-07-08 10:16:28 -0300

    Updating base for example 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,12 @@ public class Main {
         case1.addClue(new Clue("footprints found on back window", 5));
         sherlock.addCaseFile(case1);
         sherlock.solveCase(case1);
-        System.out.println("hello world");
+        
+        CaseFile case2 = new CaseFile("Bank Robbery");
+        case2.addClue(new Clue("Witness saw a black van", 3));
+        sherlock.addCaseFile((case2));
+        sherlock.solveCase(case2);
+
+        sherlock.addCaseFile(case2);
     }
 }
