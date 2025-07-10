15c4f395c00b6f9be45899661f0d7f00f159a201
ykarocin <ysa@cin.ufpe.br>
2025-07-10 00:13:03 -0300

    updating base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,11 @@ public class Main {
         
         Detective sherlock = new Detective("Sherlock Holmes");
 
-        CaseFile case1 = new CaseFile("Museum Robbery");
+        CaseFile case1 = new CaseFile(
+            "Museum Robbery"
+            );
         case1.addClue(new Clue("footprints found on back window", 5));
         sherlock.addCaseFile(case1);
         sherlock.solveCase(case1);
-        
-        CaseFile case2 = new CaseFile("Bank Robbery");
-        case2.addClue(new Clue("Witness saw a black van", 3));
-        sherlock.addCaseFile((case2));
-        sherlock.solveCase(case2);
-
-        sherlock.addCaseFile(case2);
     }
 }
